239883e65a32da547371d38d5e134edd66715db8
Ichikisota-hub <Ichikisota@icloud.com>
2026-02-15 16:41:57 +0900

    Update page.tsx

--- elevate-admin-dashboard (1)/app/players/page.tsx
+++ elevate-admin-dashboard (1)/app/players/page.tsx
@@ -79,6 +79,7 @@ const PlayersPage: React.FC = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdW1ianhoamh1cHJ3cXZxbnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4NjgxMjEsImV4cCI6MjA4NjQ0NDEyMX0._O6Q0_TDg8FfNSy444gwF7HhQxTg3hFBc5GonUeqguQ",
             "Authorization": "Bearer " + session.access_token
           },
           body: JSON.stringify({ 
