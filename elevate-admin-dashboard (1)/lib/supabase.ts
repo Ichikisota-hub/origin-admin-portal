3c2006bf0eb1f42c972496b66edffc5ff7ddabff
Ichikisota-hub <Ichikisota@icloud.com>
2026-02-15 15:32:35 +0900

    Update supabase.ts

--- elevate-admin-dashboard (1)/lib/supabase.ts
+++ elevate-admin-dashboard (1)/lib/supabase.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hdumbjxhjhuprwqvqntm.supabase.co';
-const supabaseAnonKey = 'sb_publishable_3CdNV7Fc0nEAfaktL8RChg_UWAZc1qQ';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdW1ianhoamh1cHJ3cXZxbnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4NjgxMjEsImV4cCI6MjA4NjQ0NDEyMX0._O6Q0_TDg8FfNSy444gwF7HhQxTg3hFBc5GonUeqguQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
